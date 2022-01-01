d5d91acc1b165f731f3370bcaa7046afeed9d1e9
malc0mn <malc0mn@users.noreply.github.com>
2022-01-01 18:27:53 +0100

    Added Amiibo and NTAG25 structs.

---
+++ amiibo/amiibo.go
@@ -0,0 +1,117 @@
+package amiibo
+
+import "fmt"
+
+const NTAG215Size = 540
+const AmiiboSize = 520
+
+// Amiibo is a wrapper for binary amiibo data to allow easy amiibo manipulation.
+type Amiibo struct {
+	NTAG215
+}
+
+// NewAmiibo builds a new Amiibo structure based on the given data.
+func NewAmiibo(data []byte) (*Amiibo, error) {
+	if len(data) > NTAG215Size || len(data) < AmiiboSize {
+		return nil, fmt.Errorf("amiibo: data must be > %d and < %d", AmiiboSize, NTAG215Size)
+	}
+
+	d := [NTAG215Size]byte{}
+	copy(d[:], data)
+
+	return &Amiibo{NTAG215{data: d}}, nil
+}
+
+// WriteCounter returns the amiibo write counter. This counter is also used as magic bytes to
+// create the crypto Seed.
+func (a *Amiibo) WriteCounter() []byte {
+	t := make([]byte, 2)
+	copy(t[:], a.data[17:19])
+	return t
+}
+
+// TagHMACData returns the second part of the data needed to generate the 'tag' HMAC using the
+// DerivedKey generated from the 'tag' master key (usually in a file named locked-secret.bin).
+// This is also used as the fifth data part when generating the 'data' HMAC using key
+// locked-secret.bin.
+func (a *Amiibo) TagHMACData() []byte {
+	t := make([]byte, 44)
+	copy(t[:], a.data[84:128])
+	return t
+}
+
+// TagHMAC returns the HMAC to be verified using a 'tag' DerivedKey (master key locked-secret.bin).
+func (a *Amiibo) TagHMAC() []byte {
+	t := make([]byte, 32)
+	copy(t[:], a.data[52:84])
+	return t
+}
+
+// SetTagHMAC sets the HMAC to sign the 'tag' data.
+func (a *Amiibo) SetTagHMAC(tHmac []byte) {
+	copy(a.data[52:84], tHmac[:])
+}
+
+// DataHMACData1 returns the first part of the data needed to generate the 'data' HMAC using the
+// DerivedKey generated from the 'data' master key (usually in a file named unfixed-info.bin).
+func (a *Amiibo) DataHMACData1() []byte {
+	d := make([]byte, 35)
+	copy(d[:], a.data[17:52])
+	return d
+}
+
+// DataHMACData2 returns the second part of the data needed to generate the 'data' HMAC using the
+// DerivedKey generated from the 'data' master key (usually in a file named unfixed-info.bin).
+func (a *Amiibo) DataHMACData2() []byte {
+	b := make([]byte, 360)
+	copy(b[:], a.data[160:520])
+	return b
+}
+
+// DataHMAC returns the HMAC to be verified using a 'data' DerivedKey (master key unfixed-info.bin).
+func (a *Amiibo) DataHMAC() []byte {
+	b := make([]byte, 32)
+	copy(b[:], a.data[128:160])
+	return b
+}
+
+// SetDataHMAC sets the HMAC to sign the 'data' data.
+func (a *Amiibo) SetDataHMAC(dHmac []byte) {
+	copy(a.data[128:160], dHmac[:])
+}
+
+// ID will grab the ID from the amiibo data and return it as a byte array.
+func (a *Amiibo) ID() []byte {
+	id := make([]byte, 8)
+	copy(id[:], a.data[84:92])
+	return id
+}
+
+func (a *Amiibo) XorBytes() []byte {
+	x := make([]byte, 32)
+	copy(x, a.data[96:128])
+	return x
+}
+
+// PlainData returns unencrypted Amiibo data.
+func (a *Amiibo) PlainData() []byte {
+	plain := make([]byte, 44)
+	copy(plain[:], a.data[84:128])
+	return plain
+}
+
+// CryptoSection1 returns the first block of crypto data. En/decryption must be done by
+// concatenating CryptoSection2 and en/decrypting the entire block in one go.
+func (a *Amiibo) CryptoSection1() []byte {
+	enc := make([]byte, 32)
+	copy(enc[:], a.data[20:52])
+	return enc
+}
+
+// CryptoSection2 returns the second block of crypto data. En/decryption must be done by
+// prepending CryptoSection1 and en/decrypting the entire block in one go.
+func (a *Amiibo) CryptoSection2() []byte {
+	cfg := make([]byte, 360)
+	copy(cfg[:], a.data[160:520])
+	return cfg
+}
---
+++ amiibo/ntag215.go
@@ -0,0 +1,240 @@
+package amiibo
+
+import (
+	"errors"
+	"math/rand"
+)
+
+// No attempt was made to add NTAG213 or NTAG216 support as this is out of the scope for Amiibo
+// compatibility.
+
+const (
+	// CT stands for Cascade Tag and has a fixed value of 88h as defined in ISO/IEC 14443-3 Type A.
+	CT = 0x88
+)
+
+// NTAG215 implements the NTAG215 part of the NXP Semiconductors NTAG213/215/216 specification
+// publicly available here on the NXP website: https://www.nxp.com/docs/en/data-sheet/NTAG213_215_216.pdf
+type NTAG215 struct {
+	data [540]byte
+}
+
+func NewNTAG215(data [540]byte) *NTAG215 {
+	return &NTAG215{data: data}
+}
+
+// Raw returns the raw tag data.
+func (n *NTAG215) Raw() []byte {
+	return n.data[:]
+}
+
+// UID0 returns the first byte of the seven byte serial number or UID.
+func (n *NTAG215) UID0() byte {
+	return n.data[0]
+}
+
+// UID1 returns the second byte of the seven byte serial number or UID.
+func (n *NTAG215) UID1() byte {
+	return n.data[1]
+}
+
+// UID2 returns the third byte of the seven byte serial number or UID.
+func (n *NTAG215) UID2() byte {
+	return n.data[2]
+}
+
+// BCC0 returns the first check byte of the serial number. In accordance with ISO/IEC 14443-3 it is
+// calculated as follows: BCC0 = CT ^ UID0 ^ UID1 ^ UID2
+func (n *NTAG215) BCC0() byte {
+	return n.data[3]
+}
+
+// UID3 returns the fourth byte of the seven byte serial number or UID.
+func (n *NTAG215) UID3() byte {
+	return n.data[4]
+}
+
+// UID4 returns the fifth byte of the seven byte serial number or UID.
+func (n *NTAG215) UID4() byte {
+	return n.data[5]
+}
+
+// UID5 returns the sixth byte of the seven byte serial number or UID.
+func (n *NTAG215) UID5() byte {
+	return n.data[6]
+}
+
+// UID6 returns the seventh byte of the seven byte serial number or UID.
+func (n *NTAG215) UID6() byte {
+	return n.data[7]
+}
+
+// BCC1 returns the second check byte of the serial number. In accordance with ISO/IEC 14443-3 it is
+// calculated as follows: BCC1 = UID3 ^ UID4 ^ UID5 ^ UID6
+func (n *NTAG215) BCC1() byte {
+	return n.data[8]
+}
+
+// UID returns the 7 byte UID or serial number.
+func (n *NTAG215) UID() [7]byte {
+	return [7]byte{n.UID0(), n.UID1(), n.UID2(), n.UID3(), n.UID4(), n.UID5(), n.UID6()}
+}
+
+// FullUID returns the 9 byte UID where byte 3 and 8 (the last one) are the check bits.
+func (n *NTAG215) FullUID() [9]byte {
+	return [9]byte{n.UID0(), n.UID1(), n.UID2(), n.BCC0(), n.UID3(), n.UID4(), n.UID5(), n.UID6(), n.BCC1()}
+}
+
+// SetUID sets the given UID.
+func (n *NTAG215) SetUID(uid [9]byte) error {
+	var err error
+
+	// Save old UID.
+	old := make([]byte, 9)
+	copy(old, n.data[0:9])
+
+	// Set new UID and validate.
+	copy(n.data[0:], uid[:])
+	if !n.ValidateUID() {
+		err = errors.New("amiibo: invalid UID")
+		// Restore old UID.
+		copy(n.data[0:], old[:])
+	}
+
+	return err
+}
+
+// ValidateUID validates the tag's UID or serial number in accordance with ISO/IEC 14443-3.
+func (n *NTAG215) ValidateUID() bool {
+	if n.BCC0() != CT^n.UID0()^n.UID1()^n.UID2() {
+		return false
+	}
+
+	if n.BCC1() != n.UID3()^n.UID4()^n.UID5()^n.UID6() {
+		return false
+	}
+
+	return true
+}
+
+// RandomiseUid randomises the tag's UID or serial number so that it adheres to ISO/IEC 14443-3
+// standards.
+func (n *NTAG215) RandomiseUid() error {
+	uid := make([]byte, 7)
+	if _, err := rand.Read(uid); err != nil {
+		panic("amiibo: unable to generate random bytes")
+	}
+
+	bcc := make([]byte, 2)
+	bcc[0] = CT ^ uid[0] ^ uid[1] ^ uid[2]
+	bcc[1] = uid[3] ^ uid[4] ^ uid[5] ^ uid[6]
+
+	return n.SetUID([9]byte{uid[0], uid[1], uid[2], bcc[0], uid[3], uid[4], uid[5], uid[6], bcc[1]})
+}
+
+// Int returns the second byte of page 02h and is reserved for internal data.
+func (n *NTAG215) Int() byte {
+	return n.data[9]
+}
+
+// Lock0 returns the first part of the field programmable read-only locking mechanism also referred
+// to as static lock bytes.
+func (n *NTAG215) Lock0() byte {
+	return n.data[10]
+}
+
+// Lock1 returns the second part of the field programmable read-only locking mechanism also
+// referred to as static lock bytes.
+func (n *NTAG215) Lock1() byte {
+	return n.data[11]
+}
+
+// StaticLockBytes returns the static lock bytes. The three least significant bits of lock byte 0
+// are the block-locking bits. Bit 2 is for pages 0x0a to 0x0f, bit 1 for pages 0x04 to 0x09 and
+// bit 0 deals with page 0x03 which is the capacity container.
+// A bit value of 1 represents a lock.
+func (n *NTAG215) StaticLockBytes() [2]byte {
+	return [2]byte{n.Lock0(), n.Lock1()}
+}
+
+// CapabilityContainer returns the capability container which is programmed during the IC
+// production according to the NFC Forum Type 2 Tag specification.
+// Byte 2 in the capability container defines the available memory size for NDEF (NFC Data Exchange
+// Format) messages which is 496 bytes for NTAG215.
+func (n *NTAG215) CapabilityContainer() [4]byte {
+	cc := [4]byte{}
+	copy(cc[:], n.data[12:16])
+	return cc
+}
+
+// Data returns the read/write memory of the NFC215 tag.
+func (n *NTAG215) Data() [504]byte {
+	d := [504]byte{}
+	copy(d[:], n.data[16:520])
+	return d
+}
+
+// SetData updates the entire user memory to the given byte array.
+func (n *NTAG215) SetData(d [504]byte) {
+	copy(n.data[16:], d[:])
+}
+
+// DLock0 returns the first part of the dynamic lock bytes.
+func (n *NTAG215) DLock0() byte {
+	return n.data[520]
+}
+
+// DLock1 returns the second part of the dynamic lock bytes.
+func (n *NTAG215) DLock1() byte {
+	return n.data[521]
+}
+
+// DLock2 returns the third part of the dynamic lock bytes.
+func (n *NTAG215) DLock2() byte {
+	return n.data[522]
+}
+
+// DynamicLockBytes returns the dynamic lock bytes used for locking pages starting at page 0x10 and
+// upwards which spans a memory area of 456 bytes.
+// For an Amiibo figure, this should always be 0x01 0x00 0x0f.
+func (n *NTAG215) DynamicLockBytes() [3]byte {
+	return [3]byte{n.DLock0(), n.DLock1(), n.DLock2()}
+}
+
+// CFG0 returns the NTAG215 first configuration page. This page is used to set the ASCII mirror feature.
+// For an Amiibo this should always match 0x00 0x00 0x00 0x04.
+func (n *NTAG215) CFG0() [4]byte {
+	cfg := [4]byte{}
+	copy(cfg[:], n.data[524:528])
+	return cfg
+}
+
+// CFG1 returns the NTAG215 second configuration page. This page is used to configute memory access
+// restrictions.
+// For an Amiibo this should always match 0x5f 0x00 0x00 0x00.
+func (n *NTAG215) CFG1() [4]byte {
+	cfg := [4]byte{}
+	copy(cfg[:], n.data[528:532])
+	return cfg
+}
+
+// Password returns the 32bit password used for memory access protection.
+func (n *NTAG215) Password() [4]byte {
+	pwd := [4]byte{}
+	copy(pwd[:], n.data[532:536])
+	return pwd
+}
+
+// PasswordAcknowledge returns the 16bit password acknowledge used during password verification.
+func (n *NTAG215) PasswordAcknowledge() [2]byte {
+	pack := [2]byte{}
+	copy(pack[:], n.data[536:538])
+	return pack
+}
+
+// RFUI stands for Reserved for future use - implemented. These should all be set to 0x00.
+func (n *NTAG215) RFUI() [2]byte {
+	rfui := [2]byte{}
+	copy(rfui[:], n.data[538:540])
+	return rfui
+}
