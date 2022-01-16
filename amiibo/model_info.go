6c27b6413fe13d4fc2e94f215de3c9808d838ade
malc0mn <malc0mn@users.noreply.github.com>
2022-01-16 18:05:11 +0100

    Added ModelInfo tests.

--- amiibo/model_info.go
+++ amiibo/model_info.go
@@ -2,34 +2,38 @@ package amiibo
 
 import "encoding/binary"
 
-const (
-	TypeFigure = 0x00
-	TypeCard   = 0x01
-	TypeYarn   = 0x02
+type FigureType int
 
-	SeriesSuperSmashBros        = 0x00
-	SeriesSuperMario            = 0x01
-	SeriesChibiRobo             = 0x02
-	SeriesYoshisWoollyWorld     = 0x03
-	SeriesSplatoon              = 0x04
-	SeriesAnimalCrossing        = 0x05
-	SeriesEightBitMario         = 0x06
-	SeriesSkylanders            = 0x07
-	SeriesUnknown1              = 0x08
-	SeriesTheLegendOfZelda      = 0x09
-	SeriesShovelKnight          = 0x0A
-	SeriesUnknown2              = 0x0B
-	SeriesKirby                 = 0x0C
-	SeriesPokemon               = 0x0D
-	SeriesMarioSportsSuperstars = 0x0E
-	SeriesMonsterHunter         = 0x0F
-	SeriesBoxBoy                = 0x10
-	SeriesPikmin                = 0x11
-	SeriesFireEmblem            = 0x12
-	SeriesMetroid               = 0x13
-	SeriesOthers                = 0x14
-	SeriesMegaMan               = 0x15
-	SeriesDiablo                = 0x16
+type Series int
+
+const (
+	TypeFigure FigureType = 0x00
+	TypeCard   FigureType = 0x01
+	TypeYarn   FigureType = 0x02
+
+	SeriesSuperSmashBros        Series = 0x00
+	SeriesSuperMario            Series = 0x01
+	SeriesChibiRobo             Series = 0x02
+	SeriesYoshisWoollyWorld     Series = 0x03
+	SeriesSplatoon              Series = 0x04
+	SeriesAnimalCrossing        Series = 0x05
+	SeriesEightBitMario         Series = 0x06
+	SeriesSkylanders            Series = 0x07
+	SeriesUnknown1              Series = 0x08
+	SeriesTheLegendOfZelda      Series = 0x09
+	SeriesShovelKnight          Series = 0x0A
+	SeriesUnknown2              Series = 0x0B
+	SeriesKirby                 Series = 0x0C
+	SeriesPokemon               Series = 0x0D
+	SeriesMarioSportsSuperstars Series = 0x0E
+	SeriesMonsterHunter         Series = 0x0F
+	SeriesBoxBoy                Series = 0x10
+	SeriesPikmin                Series = 0x11
+	SeriesFireEmblem            Series = 0x12
+	SeriesMetroid               Series = 0x13
+	SeriesOthers                Series = 0x14
+	SeriesMegaMan               Series = 0x15
+	SeriesDiablo                Series = 0x16
 )
 
 type ModelInfo struct {
@@ -44,25 +48,23 @@ func (mi *ModelInfo) ID() []byte {
 // GameID returns the amiibo game ID which is extracted from the full ID: the first 10 bits of the
 // first two bytes of the full amiibo ID.
 func (mi *ModelInfo) GameID() int {
-	b := binary.BigEndian.Uint16(mi.data[:2])
-	return int(b >> 6)
+	return extractBits(int(binary.BigEndian.Uint16(mi.data[:2])), 10, 0)
 }
 
 // CharacterID returns the amiibo character ID which is extracted from the full ID: the last 6 bits
 // of the first two bytes of the full amiibo ID.
 func (mi *ModelInfo) CharacterID() int {
-	b := binary.BigEndian.Uint16(mi.data[:2])
-	return int((b << 10) >> 10)
+	return extractBits(int(binary.BigEndian.Uint16(mi.data[:2])), 6, 10)
 }
 
 // CharacterVariant returns the amiibo character variant.
 func (mi *ModelInfo) CharacterVariant() int {
-	return int(binary.BigEndian.Uint16([]byte{0x00, mi.data[2]}))
+	return int(mi.data[2])
 }
 
 // FigureType returns the type of figure: TypeFigure, TypeCard or TypeYarn.
-func (mi *ModelInfo) FigureType() int {
-	return int(binary.BigEndian.Uint16([]byte{0x00, mi.data[3]}))
+func (mi *ModelInfo) FigureType() FigureType {
+	return FigureType(mi.data[3])
 }
 
 // ModelNumber returns the amiibo model number.
@@ -72,13 +74,13 @@ func (mi *ModelInfo) ModelNumber() int {
 
 // Series returns the series the amiibo is part of such as SeriesMegaMan, SeriesPokemon,
 // SeriesAnimalCrossing, etc.
-func (mi *ModelInfo) Series() int {
-	return int(binary.BigEndian.Uint16([]byte{0x00, mi.data[6]}))
+func (mi *ModelInfo) Series() Series {
+	return Series(mi.data[6])
 }
 
 // Unknown but seems to always be 0x02.
 func (mi *ModelInfo) Unknown() int {
-	return int(binary.BigEndian.Uint16([]byte{0x00, mi.data[7]}))
+	return int(mi.data[7])
 }
 
 // TODO: anyone know what the last 4 bytes 8-12 are...? (in root data bytes 92-95 or 0x5c-0x5f)
---
+++ amiibo/model_info_test.go
@@ -0,0 +1,94 @@
+package amiibo
+
+import (
+	"bytes"
+	"testing"
+)
+
+func loadModelInfo(t *testing.T) *ModelInfo {
+	data := readFile(t, "model_info.bin")
+
+	modelInfo := [12]byte{}
+	copy(modelInfo[:], data)
+	return &ModelInfo{data: modelInfo}
+}
+
+func TestModelInfo_ID(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.ID()
+	want := []byte{0x05, 0xc0, 0x00, 0x00, 0x00, 0x06, 0x00, 0x02}
+
+	if !bytes.Equal(got, want) {
+		t.Errorf("got %#x, want %#x", got, want)
+	}
+}
+
+func TestModelInfo_GameID(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.GameID()
+	want := 448
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_CharacterID(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.CharacterID()
+	want := 1
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_CharacterVariant(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.CharacterVariant()
+	want := 0
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_FigureType(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.FigureType()
+	want := TypeFigure
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_ModelNumber(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.ModelNumber()
+	want := 6
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_Series(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.Series()
+	want := SeriesSuperSmashBros
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestModelInfo_Unknown(t *testing.T) {
+	mi := loadModelInfo(t)
+	got := mi.Unknown()
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
