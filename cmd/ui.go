eea88673f330e2c29b91d025bf1cc37d02d2a76b
malc0mn <malc0mn@users.noreply.github.com>
2022-04-09 20:52:17 +0200

    Convert ui to struct.

--- cmd/ui.go
+++ cmd/ui.go
@@ -5,45 +5,77 @@ import "github.com/gdamore/tcell/v2"
 // tui is the main terminal user interface loop. It sets up a tcell.Screen, draws the UI and
 // handles UI related events.
 func tui() {
-	s, _ := initScreen()
-	drawUi(s, false)
+	ui := newUi()
+	ui.draw(true)
 
 	// Connect to the portal when the UI is visible, so it can display the client logs etc.
 	//go portal()
 
 	for {
-		s.Show()
+		ui.show()
 
-		ev := s.PollEvent()
+		ev := ui.pollEvent()
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
-			syncScreen(s)
+			ui.sync()
 		case *tcell.EventKey:
 			switch {
 			case ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC:
-				s.Fini()
+				ui.destroy()
 				close(quit)
 				return
 			case ev.Key() == tcell.KeyCtrlL:
-				syncScreen(s)
+				ui.sync()
 			}
 		}
 	}
 }
 
-// syncScreen redraws the full screen on resize or on CTRL+L.
-func syncScreen(s tcell.Screen) {
-	s.Clear()
-	drawUi(s, true)
-	s.Sync() // Is this still needed after the two preceding lines?
+// ui holds all the user interface components and the screen to render them on.
+type ui struct {
+	s     tcell.Screen
+	boxes []*box
 }
 
-// drawUi draws the entire user interface.
-func drawUi(s tcell.Screen, sync bool) {
-	width, _ := s.Size()
+// newUi create a new ui structure.
+func newUi() *ui {
+	s, _ := initScreen()
+	logs := newBox(s, 1, logoHeight()+1, 15, 30, "logs", boxWidthTypePercent)
+	return &ui{
+		s:     s,
+		boxes: []*box{logs},
+	}
+}
+
+// draw draws the entire user interface.
+func (u *ui) draw(animate bool) {
+	width, _ := u.s.Size()
 
 	x := width/2 - logoWidth()/2
-	drawLogo(s, x, 0, !sync)
-	box := newBox(s, 1, logoHeight()+1, 15, 30, "logs", boxWidthTypePercent)
-	box.draw(!sync)
+	drawLogo(u.s, x, 0, animate)
+	for _, b := range u.boxes {
+		b.draw(animate)
+	}
+}
+
+// show shows all ui content.
+func (u *ui) show() {
+	u.s.Show()
+}
+
+// sync redraws the full screen on resize.
+func (u *ui) sync() {
+	u.s.Clear()
+	u.draw(false)
+	u.s.Sync() // Is this still needed after the two preceding lines?
+}
+
+// pollEvent waits for events to arrive in the screen.
+func (u *ui) pollEvent() tcell.Event {
+	return u.s.PollEvent()
+}
+
+// destroy destroys the UI cleanly.
+func (u *ui) destroy() {
+	u.s.Fini()
 }
